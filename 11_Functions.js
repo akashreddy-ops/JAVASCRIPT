a62e8eb8b9cab3249e95d73449871a1ca79c953f
Danapana Akash Reddy <danapanaakashreddy999@gmail.com>
2025-01-13 00:03:00 +0530

    Add files via upload

---
+++ 11_Functions.js
@@ -0,0 +1,32 @@
+function sayMyName(){
+    console.log("A");
+    console.log("K");
+    console.log("A");
+    console.log("S");
+    console.log("H");
+
+}
+
+// sayMyName()
+
+
+function addTwoNumbers(number1,number2)
+{
+    console.log(number1+number2);
+}
+
+// addTwoNumbers
+
+const result= addTwoNumbers(3,4)
+
+
+// console.log("Result: ", result);
+
+
+
+function calculateCartPrice(num1){
+    return num1
+
+}
+
+console.log(calculateCartPrice(400, 300, 200))
---
+++ 12_Scope.js
@@ -0,0 +1,61 @@
+// let a = 10
+// const b = 20
+// var c = 30
+
+
+// if (true) {
+//     let a = 10
+//     const b = 20
+//     var c = 30
+// }
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+// function one(){
+//     const username = "Akash"
+
+//     function two(){
+//         const website = "Youtube"
+//         console.log(username);
+//     }
+//     // console.log(website);
+//     two()
+// }
+// one()
+
+
+if (true){
+    const username = "Akash"
+    if(username === "Akash"){
+        const website = "Youtube"
+        // console.log(username + website)
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+
+
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++ Interesting +++++++++++++++++++++++++++++++++++++++++++++++
+
+function addone(num){
+    return num + 1
+
+}
+
+console.log(addone(5))
+
+const addTwo = function(num){
+    return num + 2
+}
+
+addTwo(5)
